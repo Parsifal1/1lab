24dc6358f9d4af22d8bdabfefc2b1e855045038f
Ptize <golubevivan-99@mail.ru>
2019-03-03 16:34:53 +0300

    Создано приложение
    Консольное приложение, которое забирает координаты фигуры из файла geojson и отрисовывает их на картинке png.

---
+++ Karta/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/fogleman/gg"
+	geojson "github.com/paulmach/go.geojson"
+)
+
+func main() {
+
+	file, err := os.Open("one.geojson")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	data := make([]byte, 64)
+	raw := ""
+	for {
+		r, err := file.Read(data)
+		if err == io.EOF { // если конец файла
+			break // выходим из цикла
+		}
+		raw = raw + string(data[:r])
+	}
+
+	rawGeometryJSON := []byte(raw)
+
+	fc1, err := geojson.UnmarshalFeatureCollection(rawGeometryJSON)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	fmt.Printf("%f", fc1.Features[0].Geometry.Polygon[0][0][0])
+
+	dc := gg.NewContext(1366, 1024)
+	dc.SetHexColor("fff")
+
+	dc.InvertY()
+	dc.MoveTo(fc1.Features[0].Geometry.Polygon[0][0][0], fc1.Features[0].Geometry.Polygon[0][0][1])
+	for j := 1; j < len(fc1.Features[0].Geometry.Polygon[0]); j++ {
+		dc.LineTo(fc1.Features[0].Geometry.Polygon[0][j][0], fc1.Features[0].Geometry.Polygon[0][j][1])
+	}
+
+	dc.SetHexColor("f00")
+	dc.Fill()
+	dc.SavePNG("out.png")
+
+}
