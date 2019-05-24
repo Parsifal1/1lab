20c5c3b6d4d7824c96777a2a01471fe36c87c239
Ptize <golubevivan-99@mail.ru>
2019-05-24 04:23:09 +0300

    Выполнено дз
    ДЗ

--- main.go
+++ main.go
@@ -1,133 +1,172 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"fmt"           // пакет для форматированного ввода вывода
+	"html/template" // пакет для логирования
+	"image"
+	"image/png"
 	"io"
-	"net/http"
+	"math"
+	"math/rand"
+	"net/http" // пакет для поддержки HTTP протокола
 	"os"
 	"strconv"
 	"strings"
 
+	"github.com/davvo/mercator"
 	"github.com/fogleman/gg"
 	geojson "github.com/paulmach/go.geojson"
+	// пакет для работы с  UTF-8 строками
 )
 
-func main() {
+const width, height = 512, 512
 
-	file, err := os.Open("2_5467644889959236843.json")
+var cache map[string][]byte
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./index.html")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		fmt.Fprintf(w, err.Error())
 	}
-	defer file.Close()
+	t.ExecuteTemplate(w, "index", "hello")
+}
 
-	stat, err := file.Stat()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	data := make([]byte, 64)
-	raw := ""
-	for {
-		r, err := file.Read(data)
-		if err == io.EOF { // если конец файла
-			break // выходим из цикла
-		}
-		raw = raw + string(data[:r])
-	}
-
-	rawGeometryJSON := []byte(raw)
-
-	fc, err := geojson.UnmarshalFeatureCollection(rawGeometryJSON)
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		return
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		fmt.Println("/")
-		fmt.Println(r.URL)
-		fmt.Println(r.URL.Host)
-		srs := strings.Split(r.URL.Path, "/")
-		fmt.Println(srs)
-		srs[3] = strings.Trim(srs[3], ".png")
-		fmt.Println(srs)
-
-		const S = 100
-		dc := gg.NewContext(1366, 1024)
-		for i := 0; i < len(fc.Features); i++ {
-
-			z, _ := strconv.ParseFloat(srs[1], 64)
-			x, _ := strconv.ParseFloat(srs[2], 64)
-			y, _ := strconv.ParseFloat(srs[3], 64)
-			l := 0.0
-			if z != 0 {
-				l = (512 / (z * 2))
-			}
-
-			if fc.Features[i].Properties["layer"] == "second" && z < 5 {
-				continue
-			}
-			for r := 0; r < len(fc.Features[i].Geometry.MultiPolygon); r++ {
-				for k := 0; k < len(fc.Features[i].Geometry.MultiPolygon[r]); k++ {
-					dc.MoveTo(fc.Features[i].Geometry.MultiPolygon[r][k][0][0], fc.Features[i].Geometry.MultiPolygon[r][k][0][1])
-					dc.Push()
-
-					dc.ScaleAbout(z+1, z+1, x*l, y*l)
-					for j := 1; j < len(fc.Features[i].Geometry.MultiPolygon[r][k]); j++ {
-						dc.LineTo((fc.Features[i].Geometry.MultiPolygon[r][k][j][0]+256)/2, (fc.Features[i].Geometry.MultiPolygon[r][k][j][1]*(-1)+256)/2)
-					}
-				}
-			}
-
-			dc.SetLineWidth(10)
-			fmt.Println(fc.Features[i].Properties["color"])
-			switch fc.Features[i].Properties["color"] {
-			case "green":
-				dc.SetRGBA255(91, 255, 15, 255)
-				dc.StrokePreserve()
-				dc.SetRGBA255(91, 155, 15, 255)
-
-			case "orange":
-				dc.SetRGBA255(255, 184, 5, 255)
-				dc.StrokePreserve()
-				dc.SetRGBA255(200, 184, 5, 255)
-			default:
-				dc.SetRGBA255(255, 255, 255, 255)
-				dc.StrokePreserve()
-				dc.SetRGBA255(0, 0, 0, 255)
-			}
-
-			dc.Fill()
-			dc.Pop()
-		}
-		dc.SavePNG("out.png")
-
-		file.Close()
-
-		photo, err := os.Open("out.png")
+func draw(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.String()
+	keys := strings.Split(key, "/")
+	z, _ := strconv.ParseFloat(keys[0], 64)
+	x, _ := strconv.ParseFloat(keys[1], 64)
+	y, _ := strconv.ParseFloat(keys[2], 64)
+	var img image.Image
+	var imgBytes []byte
+	if cache[key] != nil {
+		fmt.Println("/1")
+		imgBytes = cache[key]
+		fmt.Println("/2")
+	} else {
+		file, err := os.Open("2_5467644889959236843.json")
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		defer photo.Close()
-
-		stat, err = photo.Stat()
-		if err != nil {
-			return
-		}
-		data = make([]byte, stat.Size())
+		defer file.Close()
+		fmt.Println("/")
+		data := make([]byte, 64)
+		raw := ""
 		for {
-			_, err := photo.Read(data)
+			r, err := file.Read(data)
 			if err == io.EOF { // если конец файла
 				break // выходим из цикла
 			}
+			raw = raw + string(data[:r])
 		}
-		w.Write(data)
-		w.WriteHeader(http.StatusOK)
-	})
 
-	http.ListenAndServe(":8081", nil)
+		rawGeometryJSON := []byte(raw)
+		fmt.Println("/")
+		if img, err = getPNG(rawGeometryJSON, z, x, y); err != nil {
+			fmt.Println(err.Error())
+		}
+
+		fmt.Println("/")
+		buffer := new(bytes.Buffer) //buffer - *bytes.Buffer
+		png.Encode(buffer, img)     //img - image.Image
+		imgBytes = buffer.Bytes()
+		cache[key] = imgBytes
+	}
+	fmt.Println("/")
+	w.Write(imgBytes)
+}
+
+func main() {
+	cache = make(map[string][]byte, 0)
+
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/tile/", draw)
+	http.ListenAndServe(":3000", nil)
+}
+
+func getPNG(featureCollectionJSON []byte, z float64, x float64, y float64) (image.Image, error) {
+	var coordinates [][][][][]float64
+	var err error
+	if coordinates, err = getMultyCoordinates(featureCollectionJSON); err != nil {
+		return nil, err
+	}
+	dc := gg.NewContext(width, height)
+	scale := 1.0
+	dc.InvertY()
+	//рисуем полигоны
+	forEachPolygon(dc, coordinates, func(polygonCoordinates [][]float64) {
+		dc.SetRGB(rand.Float64(), rand.Float64(), rand.Float64())
+		drawByPolygonCoordinates(dc, polygonCoordinates, scale, dc.Fill, z, x, y)
+	})
+	//рисуем контуры полигонов
+	dc.SetLineWidth(2)
+	forEachPolygon(dc, coordinates, func(polygonCoordinates [][]float64) {
+		dc.SetRGB(rand.Float64(), rand.Float64(), rand.Float64())
+		drawByPolygonCoordinates(dc, polygonCoordinates, scale, dc.Stroke, z, x, y)
+	})
+	out := dc.Image()
+	return out, nil
+}
+
+func getMultyCoordinates(featureCollectionJSON []byte) ([][][][][]float64, error) {
+	var featureCollection *geojson.FeatureCollection
+	var err error
+	if featureCollection, err = geojson.UnmarshalFeatureCollection(featureCollectionJSON); err != nil {
+		return nil, err
+	}
+	var features = featureCollection.Features
+	var coordinates [][][][][]float64
+	for i := 0; i < len(features); i++ {
+		coordinates = append(coordinates, features[i].Geometry.MultiPolygon)
+	}
+	return coordinates, nil
+}
+
+func forEachPolygon(dc *gg.Context, coordinates [][][][][]float64, callback func([][]float64)) {
+	for i := 0; i < len(coordinates); i++ {
+		for j := 0; j < len(coordinates[i]); j++ {
+			callback(coordinates[i][j][0])
+		}
+	}
+}
+
+const mercatorMaxValue float64 = 20037508.342789244
+const mercatorToCanvasScaleFactorX = float64(width) / (mercatorMaxValue)
+const mercatorToCanvasScaleFactorY = float64(height) / (mercatorMaxValue)
+
+func drawByPolygonCoordinates(dc *gg.Context, coordinates [][]float64, scale float64, method func(), z float64, xTile float64, yTile float64) {
+	scale = scale * math.Pow(2, z)
+	dx := float64(dc.Width())*(xTile) - 138.5*scale
+	dy := float64(dc.Height())*(math.Pow(2, z)-1-yTile) - 128*scale
+	for index := 0; index < len(coordinates)-1; index++ {
+		x, y := mercator.LatLonToMeters(coordinates[index][1], convertNegativeX(coordinates[index][0]))
+		x, y = centerRussia(x, y)
+		x *= mercatorToCanvasScaleFactorX * scale * 0.5
+		y *= mercatorToCanvasScaleFactorY * scale * 0.5
+		x -= dx
+		y -= dy
+		dc.LineTo(x, y)
+	}
+	dc.ClosePath()
+	method()
+}
+
+func centerRussia(x float64, y float64) (float64, float64) {
+	var west = float64(1635093.15883866)
+	if x > 0 {
+		x -= west
+	} else {
+		x += 2*mercatorMaxValue - west
+	}
+	return x, y
+}
+
+func convertNegativeX(x float64) float64 {
+	if x < 0 {
+		x = x - 360
+	}
+	return x
 }
